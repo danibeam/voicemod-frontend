efdef7bb6649155b139c91e34c96fd8b983e5392
Daniel Belmonte <danibeam97@gmail.com>
2020-12-13 14:10:49 +0100

    Filter stylings
    The dropdown and its options based in the tags taken from the services, displaying the available tags

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
+import Tags from 'services/tags';
 import './style.scss';
-import PropTypes from 'prop-types';
 
-function Filter(props) {
-  const { categories } = props;
+function Filter() {
   return (
-    <div>
+    <div className="container">
       <img alt="Filter" src="assets/filter.svg" />
       <select className="dropdown">
-        {categories.map((category) => (
-          <option>{category}</option>
+        {Tags.map((tag) => (
+          <option className="dropdown-option">{tag}</option>
         ))}
       </select>
     </div>
   );
 }
-
-Filter.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
 
 export default Filter;
