2a87ce019d04b267af7f741d3b28313ec5b78f3b
Daniel Belmonte <danibeam97@gmail.com>
2020-12-13 14:09:59 +0100

    Remove footer
    Not really needed for this exercise

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,9 +1,8 @@
 /* eslint-disable react/require-default-props */
+import Header from 'components/Header';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import Footer from 'components/Footer';
-import Header from 'components/Header';
 import './style.scss';
 
 export const Dashboard = (props) => {
@@ -22,7 +21,6 @@ export const Dashboard = (props) => {
           ))}
         </div>
       </div>
-      <Footer />
     </>
   );
 };
