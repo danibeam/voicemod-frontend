360106389a135046c292517ea657ca1a6a576450
Daniel Belmonte <danibeam97@gmail.com>
2020-12-16 18:40:06 +0100

    Overall tidy up in Dashboard
    Remove scrolling to selected voice. Tweaking the styles and overall tidy up.

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,49 +1,63 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import { ReactComponent as NonFavSvg } from 'assets/voice-favourite-off.svg';
+import { ReactComponent as FavSvg } from 'assets/voice-favourite.svg';
 import Header from 'components/Header';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
-import { connect, useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import * as actionTypes from 'store/actions/actionTypes';
 import './style.scss';
 
 export const Dashboard = (props) => {
   const { filteredVoices, selected } = props;
-  const selectedVoice = useSelector((state) => state.selected);
-  const refs = filteredVoices.reduce((acc, value) => {
-    acc[value.id] = React.createRef();
-    return acc;
-  }, {});
 
-  useEffect(() => {
-    if (selectedVoice) {
-      refs[selected.id].current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-      });
-    }
-  }, [selectedVoice]);
+  const [favs, setFavs] = useState([]);
 
   const onClickedVoice = (voice) => {
     props.onSelectedVoice(voice);
   };
+
   return (
     <>
       <Header text="Hola" />
       <div className="dashboard">
         <section>
           <h1 className="section-header">Favourite Voices</h1>
+          <div className="voices-wrapper">
+            {favs.map((favourite) => (
+              <div key={favourite.id} id={favourite.id} className="voice">
+                <img
+                  alt={favourite.icon}
+                  src={`/assets/${favourite.icon}`}
+                  onClick={() => onClickedVoice(favourite)}
+                  className={
+                    selected && selected.id === favourite.id ? 'selected' : ''
+                  }
+                />
+                <div>
+                  {selected && selected.id === favourite.id ? (
+                    <FavSvg
+                      className="fav-icon"
+                      title="Remove from the Favourites >:"
+                      onClick={() =>
+                        setFavs(favs.filter((fav) => fav.id !== favourite.id))
+                      }
+                    />
+                  ) : (
+                    ''
+                  )}
+                  {favourite.name}
+                </div>
+              </div>
+            ))}
+          </div>
         </section>
         <section id="pro-voices">
           <h1 className="section-header">Pro Voices</h1>
           <div className="voices-wrapper">
             {filteredVoices.map((voice) => (
-              <div
-                key={voice.id}
-                id={voice.id}
-                ref={refs[voice.id]}
-                className="voice"
-              >
+              <div key={voice.id} id={voice.id} className="voice">
                 <img
                   alt={voice.icon}
                   src={`/assets/${voice.icon}`}
@@ -52,7 +66,19 @@ export const Dashboard = (props) => {
                     selected && selected.id === voice.id ? 'selected' : ''
                   }
                 />
-                <span>{voice.name}</span>
+
+                <div>
+                  {selected && selected.id === voice.id ? (
+                    <NonFavSvg
+                      className="fav-icon"
+                      title="Give this voice a Fav!"
+                      onClick={() => setFavs([...favs, voice])}
+                    />
+                  ) : (
+                    ''
+                  )}
+                  {voice.name}
+                </div>
               </div>
             ))}
           </div>
